4df7964e4fdb09ef27508a1b79265e3e6ce72c90
aac-devs <aac.devs@gmail.com>
2021-03-11 08:15:10 -0300

    add line to validate-jwt

--- middlewares/validate-jwt.js
+++ middlewares/validate-jwt.js
@@ -12,6 +12,7 @@ const validateJWT = (req, res = response, next) => {
   try {
     const { uid, name } = jwt.verify(token, process.env.SECRET_JWT_SEED);
     req.uid = uid;
+    req.name = name;
   } catch (error) {
     return res.status(401).json({
       ok: false,
